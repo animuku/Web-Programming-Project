2df3efae3d8fa399cda1ad95f6dd74ba179b076b
animuku <animuku123@gmail.com>
2018-10-01 11:55:53 +0530

    Ten headlines+images

--- LoginServlet.java
+++ LoginServlet.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 public class LoginServlet extends HttpServlet {
 
- public void doPost(HttpServletRequest request, 
-  HttpServletResponse response) 
+ public void doPost(HttpServletRequest req, 
+  HttpServletResponse res) 
   throws ServletException, IOException {
 
-    String uname=request.getParameter("username");
-    String pwd=request.getParameter("password");
+    String uname=req.getParameter("username");
+    String pwd=req.getParameter("password");
     res.setContentType("text/html");
     PrintWriter out=res.getWriter();
 
     if(uname.equals("animuku")&& pwd.equals("Drogbalamp123"))
     {
-        response.sendRedirect("target.html");
+        res.sendRedirect("src/home.html");
     }
 
     else{
         out.println("Invalid username or password");
-        RequestDispatcher rs=req.RequestDispatcher("markup.html");
+        RequestDispatcher rs=req.getRequestDispatcher("src/markup.html");
         rs.forward(req,res);
     }
 
